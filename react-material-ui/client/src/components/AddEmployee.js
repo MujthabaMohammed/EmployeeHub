5c2a0e96a6f60427eac7c85b753830bdab725d08
MujthabaMohammed <49934328+MujthabaMohammed@users.noreply.github.com>
2019-05-18 13:44:49 +0530

    Update AddEmployee.js
    may 18

--- react-material-ui/client/src/components/AddEmployee.js
+++ react-material-ui/client/src/components/AddEmployee.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Formik, FormikProps, Form, Field, ErrorMessage } from 'formik';
 import { graphql, compose } from 'react-apollo';
 import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
@@ -10,10 +11,13 @@ import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import green from '@material-ui/core/colors/green';
 import AddIcon from '../icons/add.svg'
 
+import { string, url } from 'yup'; // for only what you need
+
 
 
 import { getSkillsQuery, addEmployeeMutation } from '../queries/queries';
 
+let Yup = require('yup');
 
 const theme = createMuiTheme({
   palette: {
@@ -24,18 +28,57 @@ const theme = createMuiTheme({
   },
 });
 
+
+// Defines the validationschema for the form
+
+const validationSchema = Yup.object().shape({
+  emp_name: Yup.string().required('Employee Name is required!'),
+  image: Yup.string().url('Invalid URL').required('Image is Required'),
+  company: Yup.string().required('Company Name is required!'),
+  skill : Yup.string().required('Skill Name is required!')
+   
+})
+
+
+
 class AddEmployee extends Component {
+
   constructor(props) {
     super(props);
     this.state = {
-      name: '',
-      company: '',
-      skillId: '',
-      image: 'https://i.ibb.co/nwnJgNm/none.jpg',
       open: true,
-      alert: false,
+      alert: false
     };
   }
+
+
+  handleSubmit = (values, {
+    props = this.props,
+    setSubmitting
+  }) => {
+
+    console.log(values);
+    //alert('Form Submitted');
+
+    this.props.addEmployeeMutation({
+      variables: {
+        name: values.emp_name,
+        company: values.company,
+        skillId: values.skill,
+        image: values.image
+      },
+      refetchQueries: [{
+        query: getSkillsQuery, variables: {
+          awaitRefetchQueries: true
+        }
+      }]
+    });
+    this.setState({ alert: true, open: true });
+    setSubmitting(false);
+    return;
+  }
+
+
   displaySkills() {
     var data = this.props.getSkillsQuery;
     if (data.loading) {
@@ -46,26 +89,7 @@ class AddEmployee extends Component {
       });
     }
   }
-  submitForm(e) {
-    e.preventDefault()
-    // use the addEmployeeMutation
 
-
-    this.props.addEmployeeMutation({
-      variables: {
-        name: this.state.name,
-        company: this.state.company,
-        skillId: this.state.skillId,
-        image: this.state.image
-      },
-      refetchQueries: [{
-        query: getSkillsQuery, variables: {
-          awaitRefetchQueries: true
-        }
-      }]
-    });
-    this.setState({ alert: true , open :true });
-  }
 
   handleClickOpen = () => {
     this.setState({ open: false, name: '', company: '', image: '' });
@@ -83,71 +107,116 @@ class AddEmployee extends Component {
 
   };
   render() {
+
     return (
-      <div>
+      <Formik
+        initialValues={{
+          emp_name: '',
+          company: '',
+          image: '',
+          open: true
+        }}
 
-        <Button variant="contained" color="primary" onClick={this.handleClickOpen}>Add Profile</Button>
+        validationSchema={validationSchema}
 
-        <form hidden={this.state.open} onSubmit={this.submitForm.bind(this)} >
-          <Typography gutterBottom component="h2" >
-            <img src={AddIcon} alt="na" />
+        /*validate={(values) => {
+          let errors = [];
 
-          </Typography>
-          <div className="field">
-            <label>Employee name:</label>
-            <input type="text" value={this.state.name} onChange={(e) => this.setState({ name: e.target.value })} />
-          </div>
-          <div className="field">
-            <label>Company:</label>
-            <input type="text" value={this.state.company} onChange={(e) => this.setState({ company: e.target.value })} />
-          </div>
-          <div className="field">
-            <label>Image URL:</label>
-            <input type="text" value={this.state.image} onChange={(e) => this.setState({ image: e.target.value })} />
-          </div>
-          <div className="field">
-            <label>Skill:</label>
-            <select onChange={(e) => this.setState({ skillId: e.target.value })} >
-              <option>Select Skill</option>
-              {this.displaySkills()}
-            </select>
-          </div>
+          if (!values.email)
+            errors.email = "Email Address Required";
+          console.log('See Errors Below' + errors.email);
+          //check if my values have errors
+          return errors;
+        }}*/
+        onSubmit={this.handleSubmit}
+        render={formProps => {
+          return (
 
-          <div style={{ textAlign: 'center', paddingTop: '10px' }}>
+            <div>
+              <Button variant="contained" color="primary" onClick={this.handleClickOpen}>Add Profile</Button>
+              <Form hidden={this.state.open}>
+                <Typography gutterBottom component="h2" >
+                  <img src={AddIcon} alt="na" />
 
-            <Button variant="contained" onClick={this.handleClose}>Close</Button>
-            <div className="divider" />
-            <MuiThemeProvider theme={theme} >
-              <Button variant="contained" color="primary" type="submit" >
-                Save
+                </Typography>
+                <div>
+                <Field
+                  type="text"
+                  name="emp_name"
+                  placeholder="Employee Name"
+                />
+                <ErrorMessage name="emp_name" />
+                </div>
+                <div>
+                <Field
+                  type="text"
+                  name="company"
+                  placeholder="Company"
+                />
+                <ErrorMessage name="company" />
+                </div>
+                <div>
+                <Field
+                  type="text"
+                  name="image"
+                  placeholder="Image"
+                />
+                <ErrorMessage name="image" />
+                </div>
+
+                <div>
+                <Field
+                  name="skill"
+                  component="select"
+                  placeholder="Your Skill">
+                  <option>Select Skill</option>
+                  {this.displaySkills()}
+                </Field>
+
+                <ErrorMessage name="gender" />
+
+                </div>
+                <div style={{ textAlign: 'center', paddingTop: '10px' }}>
+
+                  <Button variant="contained" onClick={this.handleClose}>Close</Button>
+                  <div className="divider" />
+                  <MuiThemeProvider theme={theme} >
+                    <Button variant="contained" color="primary" type="submit" disabled={formProps.isSubmitting}>
+                      Save
         </Button>
-            </MuiThemeProvider>
-          </div>
-        </form>
+                  </MuiThemeProvider>
+                </div>
 
 
+              </Form>
 
-        <Dialog
-          aria-labelledby="customized-dialog-title"
-          open={this.state.alert}
-        >
-          <DialogTitle id="customized-dialog-title" >
-            Alert
+              <Dialog
+                aria-labelledby="customized-dialog-title"
+                open={this.state.alert}
+              >
+                <DialogTitle id="customized-dialog-title" >
+                  Alert
           </DialogTitle>
-          <DialogContent>
-            <Typography gutterBottom>
-              New Profile Successfully Added
+                <DialogContent>
+                  <Typography gutterBottom>
+                    New Profile Successfully Added
             </Typography>
 
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={this.handleCloseAlert} color="primary">
-              Ok
+                </DialogContent>
+                <DialogActions>
+                  <Button onClick={this.handleCloseAlert} color="primary">
+                    Ok
             </Button>
-          </DialogActions>
-        </Dialog>
-      </div>
-    );
+                </DialogActions>
+              </Dialog>
+
+
+            </div>
+          );
+        }}
+      />);
+
+
   }
 }
 
